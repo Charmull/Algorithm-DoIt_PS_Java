76aa8e49432e519e2adbd417a8760deefdf24a88
jm choi <527wlals@gmail.com>
2021-03-15 20:20:14 +0900

    Chap01_Practice15

---
+++ Chap01/Practice/Practice15.java
@@ -0,0 +1,20 @@
+package Chap01.Practice;
+
+import java.util.Scanner;
+
+public class Practice15 {
+    static void triangleLB(int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        triangleLB(n);
+    }
+}

9684e2eb0a9783951bfb3b742930332bb0d20bfc
jm choi <527wlals@gmail.com>
2021-03-27 20:06:08 +0900

    Chap5_Practice05 Solution

---
+++ src/Chap05/Practice/Practice04_Solution.java
@@ -0,0 +1,52 @@
+package Chap05.Practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Practice04_Solution {
+    // 메소드 recur의 비재귀적 구현
+    static void recur3(int n) {
+        int[] nstk = new int[100];
+        int[] sstk = new int[100];
+        int ptr = -1;
+        int sw = 0;
+
+        while (true) {
+            if (n > 0) {
+                ptr++;
+                nstk[ptr] = n;
+                sstk[ptr] = sw;
+                System.out.println("sw : " + sw);
+
+                if (sw == 0)
+                    n = n - 1;
+                else if (sw == 1) {
+                    n = n - 2;
+                    sw = 0;
+                }
+                continue;
+            }
+            do {
+                n = nstk[ptr];
+                sw = sstk[ptr--] + 1;
+                System.out.println("dowhile문 sw : " + sw);
+
+                if (sw == 2) {
+                    System.out.println(n);
+                    if (ptr < 0)
+                        return;
+                }
+            } while (sw == 2);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("정수를 입력하세요. : ");
+        int x = Integer.parseInt(br.readLine());
+
+        recur3(x);
+    }
+}
